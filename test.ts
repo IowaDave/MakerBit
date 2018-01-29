061dc208ae05d58c599d5af1cd5add0e124f2c86
IowaDave <nucleorotic@gmail.com>
2018-01-28 20:53:19 -0800

    Version 2

--- test.ts
+++ test.ts
@@ -1,12 +1,12 @@
 basic.forever(() => {
     MakerBit.forwardMotorA()
     for (let q = 0; q <= 10; q++) {
-        MakerBit.motorSpeedA(1023 * ((10 - q) * (10 - q)) / 100)
+        MakerBit.motorA(1023 * ((10 - q) * (10 - q)) / 100)
         basic.pause(500)
     }
     MakerBit.reverseMotorA()
     for (let r = 0; r <= 10; r++) {
-        MakerBit.motorSpeedA(1023 * (r * r) / 100)
+        MakerBit.motorA(1023 * (r * r) / 100)
         basic.pause(500)
     }
     MakerBit.stopMotorA()
